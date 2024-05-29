d88f8f65170a731888b465136034aa8b36379ab2
Rahul Kurup <136806626+RAH07KURUP@users.noreply.github.com>
2024-05-29 10:59:36 +0530

    Update Navbutton.js
    Navbutton class when clicked

--- src/Navbutton.js
+++ src/Navbutton.js
@@ -3,8 +3,8 @@ const Navbutton = ({ind,i,text,change}) => {
 
   return (
     <div>
-        { ind!==i && <div className="nav"><Link to={`/`} onClick={()=>{change(i);}} >{text}</Link></div>}
-        { ind===i && <div className="clicked"><Link to="/" onClick={()=>{change(i);}}>{text}</Link></div>}
+        { ind!==i && <div className="nav"><Link className="nav" to={`/`} onClick={()=>{change(i);}} >{text}</Link></div>}
+        { ind===i && <div className="nav"><Link  className="clicked" to="/" onClick={()=>{change(i);}}>{text}</Link></div>}
     </div>
   );
 }
